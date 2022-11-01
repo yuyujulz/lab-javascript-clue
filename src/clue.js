b8f490951e69f8a5dc51918047651e4a1ce7693d
yuyujulz <ganina.julia2@gmail.com>
2022-11-01 22:40:43 +0000

    working on functions

--- src/clue.js
+++ src/clue.js
@@ -81,21 +81,39 @@ const roomsArray = [
 // Weapons Array
 
 const weaponsArray = [
-{name: rope, weight: 10},
-{name: knife, weight: 8},
-{name: candlestick, weight: 2},
-{name: dumbbell, weight: 30},
-{name: poison,weight: 2},
-{name: axe,weight: 15},
-{name: bat, weight: 13},
-{name: trophy,weight: 25},
-{name: pistol, weight: 20}
+{name: 'rope', weight: 10},
+{name: 'knife', weight: 8},
+{name: 'candlestick', weight: 2},
+{name: 'dumbbell', weight: 30},
+{name: 'poison',weight: 2},
+{name: 'axe',weight: 15},
+{name: 'bat', weight: 13},
+{name: 'trophy',weight: 25},
+{name: 'pistol', weight: 20}
 ];
 
 
 // ITERATION 2
 
-function selectRandom(char, room, weapon ) {}
+function selectRandom(suspectsArray, roomsArray, weaponsArray ) {
+let arrRandoms = '';
+if((suspectsArray.length === 0) && (suspectsArray.length === 0) &&(suspectsArray.length === 0)) {
+    return undefined
+}
+for (let i = 0; i < suspectsArray.firstName.lengh; i++) {
+   let arrRandoms = arrRandoms.push(math.floor(Math.random()* 7))
+    
+}
+for (let i = 0; i < roomsArray.lengh; i++) {
+   let arrRandoms = arrRandoms.push(math.floor(Math.random() * 16))
+    
+}
+for (let i = 0; i < weaponsArray.lengh; i++) {
+    let arrRandoms = arrRandoms.push(math.floor(Math.random() * 10))
+
+}
+return arrRandoms
+}
 
 function pickMystery() {}
 
